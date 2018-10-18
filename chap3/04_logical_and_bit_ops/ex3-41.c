aba09bbc55ba6bf5ed76e366dce5c4a1f8139dbd
kentakuramochi <soratobi93@gmail.com>
2018-10-19 01:45:29 +0900

    3.4

---
+++ chap3/04_logical_and_bit_ops/ex3-41.c
@@ -0,0 +1,11 @@
+// ex3-41.c
+int x, y, z;
+
+int main()
+{
+    x = 3;
+    y = 12;
+    z = x & y;
+
+    return 0;
+}
---
+++ chap3/04_logical_and_bit_ops/ex3-42.c
@@ -0,0 +1,11 @@
+// ex3-42.c
+int x, y, z;
+
+int main()
+{
+    x = 3;
+    y = 12;
+    z = x && y;
+
+    return 0;
+}
